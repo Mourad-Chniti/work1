fe98972fe86b918de3e877a4dd5ca06e3f06b97d
ZERO111 <mouradchniti9@gmail.com>
2021-11-03 12:17:56 +0100

    sarra

---
+++ BOOTSTRAP/function.js
@@ -0,0 +1,37 @@
+//Advanced 1
+//q1
+function perimeterOfCircle(rad) {
+    return 2 * Math.PI * rad 
+    }
+    //q2
+    function areaOfCircle(rad){
+        return Math.PI*rad**2
+        }
+        //q3
+        function inchesToCent(inches){
+            return inches*2.54
+            }
+            //q4
+            function inchesToCent(cent){
+                return cent/2.54
+                }
+                //q5
+                function totalSecondsConverter(hours, minutes, seconds){
+                    return hours/60+minutes*60+seconds
+                    }
+                    //q6
+                    function billTotal(number) {
+                        return number+(number*9.875/100)+(number*20/100)
+                        }
+                        //q7
+                        function convertToKilometers(miles) {
+                            return miles*1.60934
+                            }
+                            ///MORE PRACTICE 
+                            ///q1
+                            function square(n){
+                                return n**2
+                                }
+                                function cube(n){
+                                    return n**3
+                                    }
